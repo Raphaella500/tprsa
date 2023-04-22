577aa51796fa01a2123e83bb385340b53b527b05
raph <raphaellebuana@gmail.com>
2023-04-22 18:33:30 +0200

    première amélioration programme

--- src/ciphertextsecurity/Ciphertextsecurity.java
+++ src/ciphertextsecurity/Ciphertextsecurity.java
@@ -17,18 +17,53 @@ public class Ciphertextsecurity {
      * @param args the command line arguments1
      
      */
+    public static int[] H  = {6,5,2,7,4,1,3,0};
+    public static int[] PI = {4,6,0,2,7,3,1,5};
+    public static int [] P = {2,0,1,3};
     
     public static void main(String[] args) {
         
       System.out.print("PROGRAMME DE CHIFFREMENT DECHIFFREMENT DE MBUYAMBA \n");
 
+      System.out.print("AUCUNE CLE POUR LE CHIFFREMENT OU DECHIFFREMENT N'A ETE RENSEIGNE, VEUILLEEZ ENTRER UNE CLE SVP \n");
       
       String key = saisie();
       String sk[]=souscles(key);
-      String cypher=chiffrement(sk);
-      System.out.println("la première sous-clé est: " + sk[0]+ " et la deuxième sous-clé est "+sk[1]);
-      System.out.println("le message chiffré est : " + cypher);
-      System.out.println("le message chiffré est : " );    
+      System.out.println("la première sous-clé est: " + sk[0]+ " et la deuxième sous-clé est "+ sk[1]);
+      
+      
+      System.out.print("BIENVENU DANS VOTRE SIMILATEUR DE CHIFFREMENT ET DECHIFFREMENT \n");
+      
+      boolean test = true;
+      
+      while(test==true){
+          System.out.print("Que souhaitez vous faire ? Chiffrer (1) dechiffrer (0). \n");
+          Scanner console = new Scanner (System.in);
+          int avis = console.nextInt();
+          if (avis ==1){
+            String cypher=chiffrement(sk);
+            System.out.println("le message chiffré est : " + cypher);
+            }
+          else if (avis ==0){
+            String decypher=chiffrement(sk);
+            System.out.println("le message déchiffré est : " + decypher );
+            }
+          else{
+            System.out.println("Choix incorrect" );
+             }
+          System.out.print("Souhaitez vous continuer ? Tapez 1 si oui sinon n'importe quel autre chiffre \n");
+          int avis2 = console.nextInt();
+          if (avis2 !=1){
+              System.out.print("MERCI D'AVOIR UTILISE NOTRE PROGRAMME, BYE A LA PROCHAINE \n");
+              test = false;
+          }
+        
+      }
+      
+     
+      
+      
+          
         
     }
     public static String saisie(){
@@ -128,7 +163,7 @@ public class Ciphertextsecurity {
    
     
      public static String[] souscles (String key){
-        int H [] = {6,5,2,7,4,1,3,0};
+       
         key=permutation(key,H);
         String k1 = key.substring(0,4);
         String k2 = key.substring(4);
@@ -141,14 +176,13 @@ public class Ciphertextsecurity {
         subkey[1]=sk2;
         return subkey;
     }
+     
     public static String chiffrement(String[] sk){
         System.out.print("Saisissez svp le bloc de 8 bits à chiffrer: \n");
         String N=saisie();
-        int[] PI = {4,6,0,2,7,3,1,5};
         N=permutation(N,PI);
         String G0=N.substring(0,4);
         String D0=N.substring(4);
-        int [] P = {2,0,1,3};
        
         // Premier round
         String D1=permutation(G0,P);
@@ -169,6 +203,28 @@ public class Ciphertextsecurity {
         
         return cypher;
         
+    }
+    public static String dechiffrement ( String []sk){
+        
+       String C =saisie(); 
+       C = permutation(C,PI);
+       int[] P_1 = inversepermutation(P);
+       String G2 = C.substring(0,4);
+       String D2 = C.substring(4);
+       // Premier round
+       String G1= Ouexclusif(D2, sk[1]);
+       G1 = permutation (G1, P_1);
+      String D1 = Oulogique(G1, sk [1]);
+      D1 = Ouexclusif(D1, G2);
+      //Deuxième round
+      String G0 = Ouexclusif(D1, sk[0]);
+      G0 = permutation(G0, P_1);
+      String D0 =Oulogique(G0, sk[0]);
+      D0 = Ouexclusif(G1, D0);
+      String decypher = G0 + D0;
+      int[] PI_1 = inversepermutation (PI);
+      decypher = permutation (decypher, PI_1);
+      return decypher ;
     }
     }
     
